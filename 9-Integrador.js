b913245003a6acdea0378c8ab0db1b78b34578ed
oscar <oschneider742@gmail.com>
2026-05-28 10:04:45 -0300

    fix: se corrigio el punto 9

--- 9-Integrador.js
+++ 9-Integrador.js
@@ -1,3 +1,22 @@
 const nombreAlumno = 'Jose Perez';
-let notas = [7, 6, 9, 8, 8, 7]
+let notas = [6, 6, 2, 4, 2, 8]
 let sumaNotas = 0;
+let notaTotal;
+let estado = ''
+
+for (let index = 0; index < notas.length; index++) {
+    sumaNotas = sumaNotas + notas[index];
+}
+
+notaTotal = sumaNotas / notas.length
+notaTotal = notaTotal.toFixed(0);
+
+if (notaTotal == 6) {
+    estado = 'Aprobado';
+} else if (notaTotal >= 7) {
+    estado = 'Muy Bueno';
+} else {
+    estado = 'Desaprobado';
+}
+
+console.log(`El Alumno ${nombreAlumno} su promedio es: ${notaTotal}, ${estado}`);
